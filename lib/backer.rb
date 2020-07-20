f1009006c5a177c8e407bf57e2df209bb2b67cf0
Jessie Donovan <donovanajessie@gmail.com>
2020-07-20 02:11:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,10 +5,6 @@ class Backer
   def initialize(name)
     @name = name
       @backed_projects = []
-  end
-  def back_project(project)
-  @backed_projects << project
-  project.backers << self
 end
 
 end
